669f9c559b12b0ac439d86ca4b6632a6e0e72491
Thomas Fiedler <thfied@gmail.com>
2015-02-09 21:45:29 +0100

    #14 Remove reference to connect-livereload from Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,4 @@
 'use strict';
-var LIVERELOAD_PORT = 35729;
-var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
-var mountFolder = function (connect, dir) {
-    return connect.static(require('path').resolve(dir));
-};
 
 module.exports = function (grunt) {
 
