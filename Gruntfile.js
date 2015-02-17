fb0aa775f397786958be12bd4f2375225d389fb6
flashback2k14 <feedbackcalcest@gmail.com>
2015-02-17 20:44:29 +0100

    #19 first commit for buildsystem
    see comments in gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,43 +1,355 @@
 'use strict';
-var LIVERELOAD_PORT = 35729;
-var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
-var mountFolder = function (connect, dir) {
-    return connect.static(require('path').resolve(dir));
-};
 
 module.exports = function (grunt) {
+  //show elapsed time at the end
+  require('time-grunt')(grunt);
+  //load all grunt tasks
+  require('load-grunt-tasks')(grunt);
+  grunt.loadNpmTasks('web-component-tester');
 
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
-    var yeomanConfig = {
-        app: 'app',
-        dist: 'dist'
-    };
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        'wct-test': {
-            local: {
-                options: {remote: false}
-            },
-            remote: {
-                options: {remote: true}
-            },
-            chrome: {
-                options: {browsers: ['chrome']}
+  /**
+   * configurable paths
+   */
+  var yeomanConfig = {
+    app: 'app',
+    dist: 'dist'
+  };
+
+  /**
+   * Grunt Config
+   */
+  grunt.initConfig({
+    yeoman: yeomanConfig,
+    /**
+     * Watch
+     */
+    watch: {
+      options: {
+        nospawn: true
+      },
+      default: {
+        files: [
+          '<%= yeoman.app %>/*.html',
+          '<%= yeoman.app %>/elements/{,*/}*.html',
+          '{.tmp,<%= yeoman.app %>}/elements/{,*/}*.{css,js}',
+          '{.tmp,<%= yeoman.app %>}/styles/{,*/}*.css',
+          '{.tmp,<%= yeoman.app %>}/scripts/{,*/}*.js',
+          '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+        ]
+      },
+      js: {
+        files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
+        tasks: ['jshint']
+      },
+      styles: {
+        files: [
+          '<%= yeoman.app %>/styles/{,*/}*.css',
+          '<%= yeoman.app %>/elements/{,*/}*.css'
+        ],
+        tasks: ['copy:styles', 'autoprefixer:server']
+      },
+      json: {
+        files: ['<%= yeoman.app %>/manifests/{,*/}*.json']
+      }
+    },
+    /**
+     * Autoprefixer
+     */
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 versions']
+      },
+      server: {
+        files: [{
+          expand: true,
+          cwd: '.tmp',
+          src: '**/*.css',
+          dest: '.tmp'
+        }]
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= yeoman.dist %>',
+          src: ['**/*.css', '!bower_components/**/*.css'],
+          dest: '<%= yeoman.dist %>'
+        }]
+      }
+    },
+    /**
+     * BrowserSync
+     */
+    browserSync: {
+      options: {
+        notify: false,
+        port: 9000,
+        open: true
+      },
+      app: {
+        options: {
+          watchTask: true,
+          injectChanges: false, // can't inject Shadow DOM
+          server: {
+            baseDir: ['.tmp', '<%= yeoman.app %>'],
+            routes: {
+              '/bower_components': 'bower_components'
             }
+          }
+        },
+        src: [
+          '.tmp/**/*.{css,html,js}',
+          '<%= yeoman.app %>/**/*.{css,html,js}'
+        ]
+      },
+      dist: {
+        options: {
+          server: {
+            baseDir: 'dist'
+          }
+        },
+        src: [
+          '<%= yeoman.dist %>/**/*.{css,html,js}',
+          '!<%= yeoman.dist %>/bower_components/**/*'
+        ]
+      }
+    },
+    /**
+     * Clean
+     */
+    clean: {
+      dist: ['.tmp', '<%= yeoman.dist %>/*'],
+      server: '.tmp'
+    },
+    /**
+     * Jshint
+     */
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc',
+        reporter: require('jshint-stylish')
+      },
+      all: [
+        //'<%= yeoman.app %>/scripts/{,*/}*.js'   --> Failed
+        //'!<%= yeoman.app %>/scripts/vendor/*',  --> needed?
+        //'test/spec/{,*/}*.js'                   --> needed?
+      ]
+    },
+    /**
+     * UseminPrepare
+     */
+    useminPrepare: {
+      html: '<%= yeoman.app %>/index.html',
+      options: {
+        dest: '<%= yeoman.dist %>'
+      }
+    },
+    /**
+     * Usemin
+     */
+    usemin: {
+      html: ['<%= yeoman.dist %>/{,*/}*.html'],
+      css: ['<%= yeoman.dist %>/styles/{,*/}*.css'],
+      options: {
+        dirs: ['<%= yeoman.dist %>']
+      }
+    },
+    /**
+     * Replace
+     */
+    replace: {
+      dist: {
+        options: {
+          patterns: [{
+            match: /\/elements\/socobo-imports\/socobo-imports\.html/g,
+            replacement: '/elements/socobo-imports/socobo-imports.vulcanized.html'
+          }]
+        },
+        files: {
+          '<%= yeoman.dist %>/index.html': ['<%= yeoman.dist %>/index.html']
         }
-    });
+      }
+    },
+    /**
+     * Vulcanize
+     */
+    vulcanize: {
+      default: {
+        options: {
+          strip: true
+        },
+        files: {
+          '<%= yeoman.dist %>/elements/socobo-imports/socobo-imports.vulcanized.html': [
+            '<%= yeoman.dist %>/elements/socobo-imports/socobo-imports.html'
+          ]
+        }
+      }
+    },
+    /**
+     * Imagemin
+     */
+    imagemin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= yeoman.app %>/images',
+          src: '{,*/}*.{png,jpg,jpeg,svg}',
+          dest: '<%= yeoman.dist %>/images'
+        }]
+      }
+    },
+    /**
+     * cssmin
+     */
+    cssmin: {
+      main: {
+        files: {
+          '<%= yeoman.dist %>/styles/main.css': [
+            '.tmp/concat/styles/{,*/}*.css'
+          ]
+        }
+      },
+      elements: {
+        files: [{
+          expand: true,
+          cwd: '.tmp/elements',
+          src: '{,*/}*.css',
+          dest: '<%= yeoman.dist %>/elements'
+        }]
+      }
+    },
+    /**
+     * MinifyHtml
+     */
+    minifyHtml: {
+      options: {
+        quotes: true,
+        empty: true,
+        spare: true
+      },
+      app: {
+        files: [{
+          expand: true,
+          cwd: '<%= yeoman.dist %>',
+          src: '*.html',
+          dest: '<%= yeoman.dist %>'
+        }]
+      }
+    },
+    /**
+     * Copy
+     */
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%= yeoman.app %>',
+          dest: '<%= yeoman.dist %>',
+          src: [
+            '*.{ico,txt}',
+            '.htaccess',
+            '*.html',
+            'elements/**',
+            'manifests/**',
+            'scripts/**',
+            '!elements/**/*.css',
+            'images/{,*/}*.{webp,gif}'
+          ]
+        }, {
+          expand: true,
+          dot: true,
+          dest: '<%= yeoman.dist %>',
+          src: ['bower_components/**']
+        }]
+      },
+      styles: {
+        files: [{
+          expand: true,
+          cwd: '<%= yeoman.app %>',
+          dest: '.tmp',
+          src: ['{styles,elements}/{,*/}*.css']
+        }]
+      }
+    },
+    /**
+     * Tests
+     */
+    'wct-test': {
+      local: {
+        options: {remote: false}
+      },
+      remote: {
+        options: {remote: true}
+      }
+    },
+    /**
+     * Pagespeed
+     */
+    pagespeed: {
+      // See this tutorial if you'd like to run PageSpeed
+      // against localhost: http://www.jamescryer.com/2014/06/12/grunt-pagespeed-and-ngrok-locally-testing/
+      options: {
+        // By default, we use the PageSpeed Insights
+        // free (no API key) tier. You can use a Google
+        // Developer API key if you have one. See
+        // http://goo.gl/RkN0vE for info
+        nokey: true
+      },
+      // Update `url` below to the public URL for your site
+      mobile: {
+        options: {
+          url: "https://socobo.herokuapp.com/",
+          locale: "en_US",
+          strategy: "mobile",
+          threshold: 80
+        }
+      }
+    }
+  });
 
-    // Load plugins
-    grunt.loadNpmTasks('web-component-tester');
+  /**
+   * Grunt Tasks
+   */
+  grunt.registerTask('server', function (target) {
+    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
+    grunt.task.run(['serve:' + target]);
+  });
 
-    grunt.registerTask('test', [
-        'wct-test:chrome'
+  grunt.registerTask('serve', function (target) {
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'browserSync:dist']);
+    }
+
+    grunt.task.run([
+      'clean:server',
+      'copy:styles',
+      'autoprefixer:server',
+      'browserSync:app',
+      'watch'
     ]);
+  });
 
-    grunt.registerTask('test-local', [
-        'wct-test:local'
-    ]);
+  grunt.registerTask('test:local', ['wct-test:local']);
+  grunt.registerTask('test:remote', ['wct-test:remote']);
 
+  grunt.registerTask('build', [
+    'clean:dist',
+    'copy',
+    //'useminPrepare', --> Failed
+    //'imagemin',      --> Failed
+    //'concat',        --> Failed
+    'autoprefixer',
+    //'uglify',        --> Failed
+    //'cssmin',        --> Failed
+    //'vulcanize',     --> Failed
+    //'usemin',        --> Failed
+    'replace',
+    'minifyHtml'
+  ]);
+
+  grunt.registerTask('default', [
+    'jshint',
+    //'test:local',    --> Failed
+    'build'
+  ]);
 };
-
---
+++ app/scripts/main.js
@@ -0,0 +1,28 @@
+function getCookie() {
+  return window.authToken;
+}
+
+function getCookieFromDOM() {
+  var name = 'authToken=';
+  var ca = document.cookie.split(';');
+  if(ca.length > 0) {
+    for(var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) === ' ') {
+        c = c.substring(1);
+        if (c.indexOf(name) === 0) {
+          return c.substring(name.length, c.length);
+        }
+      }
+    }
+  }
+  return '';
+}
+
+function isAuthTokenAvailable() {
+  return (getCookie() !== null);
+}
+
+function setCookie() {
+  window.authToken = getCookieFromDOM();
+}
